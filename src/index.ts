a9ff4cf58b9b7abcca0d6ea8acff157af06e522a
Indra Agus Lesmana <indra953@gmail.com>
2026-08-16 14:11:03 +0700

    feat: add api login

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { neon } from "@neondatabase/serverless";
+import { and, eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/neon-http";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
@@ -40,6 +41,29 @@ app.post("/api/auth/register", async (c) => {
       .returning();
 
     return c.json({ success: true, data: newUser[0] }, 201);
+  } catch (err: any) {
+    return c.json({ success: false, message: err.message }, 500);
+  }
+});
+
+app.post("/api/auth/login", async (c) => {
+  const db = getDb();
+  const { nipNik, password } = await c.req.json();
+
+  try {
+    const foundUsers = await db
+      .select()
+      .from(users)
+      .where(and(eq(users.nipNik, nipNik), eq(users.passwordHash, password)));
+
+    if (foundUsers.length === 0) {
+      return c.json(
+        { success: false, message: "NIP/NIK atau password salah" },
+        401,
+      );
+    }
+
+    return c.json({ success: true, data: foundUsers[0] }, 200);
   } catch (err: any) {
     return c.json({ success: false, message: err.message }, 500);
   }
