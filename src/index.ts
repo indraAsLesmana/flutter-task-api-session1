929ad911e05efeb9faf184026799b1a904b135b7
Indra Agus Lesmana <indra953@gmail.com>
2026-08-16 13:28:26 +0700

    feat: add register api

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { classes } from "./db/schema";
+import { classes, users } from "./db/schema";
 
 const app = new Hono();
 app.use("*", cors());
@@ -17,6 +17,29 @@ app.get("/api/classes", async (c) => {
   try {
     const data = await db.select().from(classes);
     return c.json({ success: true, data });
+  } catch (err: any) {
+    return c.json({ success: false, message: err.message }, 500);
+  }
+});
+
+app.post("/api/auth/register", async (c) => {
+  const db = getDb();
+  const { nama, role, nipNik, email, password, classId } = await c.req.json();
+
+  try {
+    const newUser = await db
+      .insert(users)
+      .values({
+        nama,
+        role,
+        nipNik,
+        email: email || null,
+        passwordHash: password,
+        classId: role === "guru" ? null : classId,
+      })
+      .returning();
+
+    return c.json({ success: true, data: newUser[0] }, 201);
   } catch (err: any) {
     return c.json({ success: false, message: err.message }, 500);
   }
